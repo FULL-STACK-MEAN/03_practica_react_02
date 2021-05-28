bfa4406e29dcf0f349035c588a49fa2939e978e2
getafemean <getafemean@gmail.com>
2021-05-28 19:19:32 +0200

    tasks

--- src/App.js
+++ src/App.js
@@ -3,11 +3,38 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  tasks = [
+    {
+      id: 1,
+      title: 'Contactar Repsol',
+      members: 'Juan López',
+      description: 'Contactar con el departamento de energía',
+      date: '2021-06-05'
+    },
+    {
+      id: 2,
+      title: 'Preparar dossier Jazztel',
+      members: 'Laura Gómez',
+      description: 'Preparar dossier de blablabla...',
+      date: '2021-06-10'
+    },
+    {
+      id: 3,
+      title: 'Cierre cuenta Telefónica',
+      members: 'Lucía Pérez',
+      description: 'Cierre trimestral blablabla...',
+      date: '2021-06-30'
+    }
+  ]
+
   render() {
     return (
-      <div>
-        <h1>¡Hola Mundo!</h1>
-      </div>
+      <>
+        { this.tasks.map((elem, i) => {
+          return <p key={i}>{elem.title} - {elem.members} - {elem.date}</p>
+        })}
+      </>
     )
   }
 }
