5d575f8742a719e6a35c7f4dde6d773fb6f8fbd1
getafemean <getafemean@gmail.com>
2021-05-28 18:50:48 +0200

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Empleo de sass en react
+
+Usa librería node-sass (para evitar problemas de compatibilidad en react usar version 4.14.1)
+
+ó
+
+Usa directamente sass (npm i sass --save) // All credits to Enrique
---
+++ src/App.js
@@ -0,0 +1,16 @@
+// Ejemplo de componente raiz de clase
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>¡Hola Mundo!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
+
