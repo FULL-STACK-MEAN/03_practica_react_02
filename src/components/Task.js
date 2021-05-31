2c06c9cc75e9a6362446e3c3e4ff4e902880def3
getafemean <getafemean@gmail.com>
2021-05-31 19:58:45 +0200

    styling card

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import trashIcon from '../img/icons/trash.svg';
+import usersIcon from '../img/icons/users.svg';
+import calendarIcon from '../img/icons/calendar.svg';
 
 class Task extends Component {
     render() {
@@ -9,11 +12,20 @@ class Task extends Component {
             <div className="card">
                 <div className="header-card">
                     <h1>{task.title}</h1>
+                    <img src={trashIcon} alt="" />
                 </div>
                 <div className="body-card">
-                    <p>{task.members}</p>
-                    <p>{task.description}</p>
-                    <p>{task.date}</p>
+                    <div>
+                        <p className="users">
+                            <img className="m-r-small" src={usersIcon} alt="" />
+                            {task.members}
+                        </p>
+                        <p className="description">{task.description}</p>
+                    </div>
+                    <p className="date">
+                        <img className="m-r-small" src={calendarIcon} alt="" />
+                        {task.date}
+                    </p>
                 </div>
                 <div className="footer-card">
                     <button>Pendiente</button>
